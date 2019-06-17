3ef15ef97c3158bef608be14b4e0a752015e87fe
tingyi198 <tingyi198@gmail.com>
2019-06-17 22:22:09 +0800

    顯示上傳的圖片

--- app/User.php
+++ app/User.php
@@ -42,8 +42,8 @@ class User extends Authenticatable
         return $this->hasOne(Profile::class);
     }
 
-    public function posts ()
+    public function posts()
     {
-        return $this->hasMany(Post::class);
+        return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,30 +9,29 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1> {{ $user->username }} </h1>
-                <a href="#">Add New Post</a>
+                <a href="/p/create">Add New Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>352</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>637k</strong> followers</div>
                 <div class="pr-5"><strong>380</strong> following</div>
             </div>
 
-            <div class="pt-4 font-weight-bold"> {{ $user->profile->title }} </div>
-            <div> {{ $user->profile->description }} </div>
+            <div class="pt-4 font-weight-bold"> {{ $user->profile->title ?? '' }} </div>
+            <div> {{ $user->profile->description ?? '' }} </div>
             <div><a href="#"> {{ $user->profile->url ?? '' }} </a></div>
         </div>
     </div>
 
     <div class="row pt-4">
-        <div class="col-4">
-            <img class="w-100" src="https://instagram.ftpe10-1.fna.fbcdn.net/vp/d208172a3a62064f711d85097cb800b1/5D8B454B/t51.2885-15/sh0.08/e35/c0.74.877.877a/s640x640/61129659_484895502256050_2457635576497186056_n.jpg?_nc_ht=instagram.ftpe10-1.fna.fbcdn.net" alt="">
+
+        @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+            <img class="w-100" src="/storage/{{ $post->image }}" alt="">
         </div>
-        <div class="col-4">
-            <img class="w-100" src="https://instagram.ftpe10-1.fna.fbcdn.net/vp/171abd36f664985b9f1418d3af058b3b/5D93E39D/t51.2885-15/sh0.08/e35/s640x640/46113827_2143101105742339_7901500110750255475_n.jpg?_nc_ht=instagram.ftpe10-1.fna.fbcdn.net" alt="">
-        </div>
-        <div class="col-4">
-            <img class="w-100" src="https://instagram.ftpe10-1.fna.fbcdn.net/vp/131cb757a10e2e8545229610b784690a/5D80F8D9/t51.2885-15/e35/c94.0.561.561a/41166694_287344818539343_4615446525717021691_n.jpg?_nc_ht=instagram.ftpe10-1.fna.fbcdn.net" alt="">
-        </div>
+        @endforeach
+
+
     </div>
 </div>
 @endsection
